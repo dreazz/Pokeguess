4d8e56bfa5d41827e892d3958d224d6fc1d9717e
dreazz <agustinsilveira.96@gmail.com>
2019-01-24 10:30:20 +0100

    fix button menu

--- src/game.js
+++ src/game.js
@@ -3,6 +3,7 @@ var main = document.querySelector(".main");
 var blurred = document.querySelector(".blurred");
 var pokemonSection = document.querySelector(".screen-main");
 var scoreList = document.getElementsByClassName("dropdown-score");
+var startButton = document.querySelector("#menu-button");
 
 function Game() {
   this.pokemon = new Pokemon();
@@ -37,18 +38,52 @@ Game.prototype.nameCheck = function() {
     }
   }
 };
-Game.prototype.getHighScore = function(){
-  
-  for(var i in localStorage){
-    scoreList.innerHTML = `<li class="score"> <h3>${localStorage.key(i)}</h3><h3>${localStorage[localStorage.key(i)]}</h3></li> `;
+Game.prototype.getHighScore = function() {
+  for (var i in localStorage) {
+    scoreList.innerHTML = `<li class="score"> <h3>${localStorage.key(
+      i
+    )}</h3><h3>${localStorage[localStorage.key(i)]}</h3></li> `;
   }
-}
+};
 //Sets pokemon data from the api
 Game.prototype.setPokemon = function(data) {
   this.pokemon.setName(data.name);
   this.pokemon.setSprite(data.sprites.front_default);
 };
+//printStartScreen declaration
+Game.prototype.printStartScreen = function() {
+  pokemonSection.innerHTML = ` 
+  
+  <form class="form">
+    <h1>PokeGuess</h1>
+    <label for="username">Username</label> 
+    <input type="text" class="user-input player-name" placeholder="ash" autofocus/>
+    <button class="startButton btn">Start Game</button>
+  </form>
+<ul>
+  <li class="rules">
+    <i class="fas fa-scroll"></i>
+    <div class="dropdown-rules dropdown">
+      <h3>Rules</h3>
+      <p> Guess the pokemon that will be shown </p>
+      <p>You have 3 trys before you die, i mean ... you lose :)</p>
+      <p>Easy Peasy Lemon Squeezy</p>
+    </div>
+  </li>
+  <li class="high-score">
+      <i class="fas fa-trophy"></i>
+      <div class="dropdown-score dropdown">
+      <h3>Highscore</h3>
+      <ul class="high-score-list">
+      
+      </ul>
+      </div>
+    </i>
+  </li>
+</ul>
 
+`;
+}
 Game.prototype.printGameScreen = function() {
   console.log(this.pokemon.name + " " + this.isStarted);
   pokemonSection.innerHTML = `  
@@ -63,7 +98,7 @@ Game.prototype.printGameScreen = function() {
     <img class="blurred" src ="${this.pokemon.sprite}"> 
     </div> 
     <form name="myForm" id="pokeForm" class="form" onsubmit="return game.nameCheck()">
-     <input type="text" class="user-input" name="pokeName" placeholder="pikcachu" >
+     <input type="text" class="user-input" name="pokeName" placeholder="pikachu" >
     </form> 
     </div>
  `;
@@ -72,12 +107,7 @@ Game.prototype.printGameScreen = function() {
 Game.prototype.printGameOverScreen = function() {
   game.isStarted = false;
   if (this.player.points === this.maxPoints && this.player.lives > 0) {
-    setTimeout(confetti,800);
-    
-    startButton.addEventListener("click", function() {
-  
-      game.printGameScreen();
-    });
+    setTimeout(confetti, 800);
 
     pokemonSection.innerHTML = `
 
@@ -99,21 +129,19 @@ Game.prototype.printGameOverScreen = function() {
       <img src="${this.pokemon.hallOfFame[5].sprite}" >
     </div>
     <!--<button class="startButton btn">Play Again</button>-->
-    <button class="startButton btn">Menu</button>
+    <button class="startButton btn" onclick="game.printStartScreen()">Menu</button>
   `;
-   localStorage.setItem(this.player.name, this.player.points);
+
+    localStorage.setItem(this.player.name, this.player.points);
   } else {
-    startButton.addEventListener("click", function() {
-  
-      game.printGameScreen();
-    });
     pokemonSection.innerHTML = `
   
-  <h1 class="header-game-over"> you suck so bad that you hurted yourself</h1>
+  <h1 class="header-game-over"> you suck so bad that you hurt yourself</h1>
     <img class="img-game-over" src="https://pbs.twimg.com/profile_images/498674423357251585/cD4IfiPQ_400x400.jpeg">
     <!--<button class="startButton btn">Play Again</button>-->
-    <button class="startButton btn">Menu</button>
+    <button class="startButton btn" onclick="game.printStartScreen()">Menu</button>
   `;
-  localStorage.setItem(this.player.name, this.player.points);
+
+    localStorage.setItem(this.player.name, this.player.points);
   }
 };
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ var game;
 var startButton;
 var playerName = document.querySelector(".player-name");
 
-window.addEventListener("load", function() {
+ window.addEventListener("load", function() {
   game = new Game();
   apiCall(); // call the api so i have the data for the game screen
   game.getHighScore();
-  printStartScreen();
+  game.printStartScreen();
 
   startButton = document.querySelector(".startButton");
   playerName = document.querySelector(".player-name");
@@ -18,38 +18,5 @@ window.addEventListener("load", function() {
     game.printGameScreen();
   });
 
-  //printStartScreen declaration
-  function printStartScreen() {
-    pokemonSection.innerHTML = ` 
-    
-    <form class="form">
-      <h1>PokeGuess</h1>
-      <label for="username">Username</label> 
-      <input type="text" class="user-input player-name" placeholder="ash" autofocus/>
-      <button class="startButton btn">Start Game</button>
-    </form>
-  <ul>
-    <li class="rules">
-      <i class="fas fa-scroll"></i>
-      <div class="dropdown-rules dropdown">
-        <h3>Rules</h3>
-        <p> Guess the pokemon that will be shown </p>
-        <p>You have 3 trys before you die, i mean ... you lose :)</p>
-        <p>Easy Peasy Lemon Squeezy</p>
-      </div>
-    </li>
-    <li class="high-score">
-        <i class="fas fa-trophy"></i>
-        <div class="dropdown-score dropdown">
-        <h3>Highscore</h3>
-        <ul class="high-score-list">
-        
-        </ul>
-        </div>
-      </i>
-    </li>
-  </ul>
   
-  `;
-  }
 });
